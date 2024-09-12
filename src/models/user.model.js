9cdb0c46d4df8460513ffbb188645e5322783656
sumanpreetkaur4752 <sumanbhullar4752@gmail.com>
2024-09-12 21:24:28 +0530

    add register controller and bug fixes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -63,7 +63,7 @@ userSchema.methods.isPasswordCorrect = async function(password){
 
 userSchema.methods.generateAccessToken = function(){
   return jwt.sign(
-    //payload
+    //payload- Data
     {
       _id:this._id,
       email:this.email,

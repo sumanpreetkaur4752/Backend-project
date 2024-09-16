1b8f6e85e2e1420f550ae7775b78c3692bea49d6
sumanpreetkaur4752 <sumanbhullar4752@gmail.com>
2024-09-16 18:06:19 +0530

    Add subscription model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -185,7 +185,8 @@ const logoutUser = asyncHandler(async(req,res)=>{
 
 const refreshAccessToken = asyncHandler(async(req,res)=>{
          const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
-
+        //  console.log(incomingRefreshToken);
+         
          if (!incomingRefreshToken) {
             throw new ApiError(401,"unauthorized request")
          }
@@ -194,7 +195,8 @@ const refreshAccessToken = asyncHandler(async(req,res)=>{
             const decodedToken=jwt.verify(incomingRefreshToken,process.env.REFRESH_TOKEN_SECRET)
    
             const user = await User.findById(decodedToken?._id)
-   
+            // console.log(user);
+            
             if (!user) {
                throw new ApiError(401," Invalid refresh token")
             }
